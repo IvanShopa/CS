c74e357057780eb222a68ef2f54948a9b2786ebb
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-08-31 09:55:42 +0300

    Update Calendar.js

--- Home/CS-22-1/Calendar/Calendar.js
+++ Home/CS-22-1/Calendar/Calendar.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   else {
-    resultText += "-";
+    resultText += "—";
   }
   document.getElementById("weekResult").innerHTML = resultText;
 });
